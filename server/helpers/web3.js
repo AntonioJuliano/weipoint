cb8cf597685a67c663fd41bdb5a1c95e8ddeeba0
Antonio Juliano <antonio.m.juliano@gmail.com>
2017-02-09 08:59:13 -0800

    parity through env var

--- server/helpers/web3.js
+++ server/helpers/web3.js
@@ -1,7 +1,14 @@
 const Web3 = require("web3");
 const web3 = new Web3();
 const bluebirdPromise = require("bluebird");
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+const logger = require('./logger');
+const providerPath = process.env.PARITY_URL;
+logger.info({
+  at: 'web3#connect',
+  message: 'Connecting to web3 provider',
+  url: providerPath
+})
+web3.setProvider(new web3.providers.HttpProvider(providerPath));
 bluebirdPromise.promisifyAll(web3.eth);
 bluebirdPromise.promisifyAll(web3.eth.compile);
 
