61bcf7947f7e9318abcba6db2df51653dd912335
Antonio Juliano <antonio.m.juliano@gmail.com>
2017-02-11 20:44:41 -0800

    redesign search result

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,37 +1,50 @@
-/**
- * Created by antonio on 1/2/17.
- */
-import * as React from "react";
-import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
+import React from "react";
+import {Card, CardActions, CardTitle} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import '../styles/SearchResult.css';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+const initialBytecodeButtonText = "Copy Bytecode";
 
 class SearchResult extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bytecodeButtonText: initialBytecodeButtonText
+    };
+    this.copyBytecodeClicked = this.copyBytecodeClicked.bind(this);
+  }
 
-    render() {
-        return (
-            <div className="SearchResultContainer">
-                <Card>
-                    <CardTitle
-                      title="Contract"
-                      subtitle={this.props.contract.address}
-                    />
-                    <CardText>
-                        {this.props.contract.code}
-                    </CardText>
-                    <CardActions>
-                        <FlatButton label="Upload Source" />
-                        <FlatButton label="Send Transaction" />
-                    </CardActions>
-                </Card>
-            </div>
-        );
-    }
+  copyBytecodeClicked(e) {
+    console.log(this);
+    this.setState({ bytecodeButtonText: 'Copied!' });
+    const thisRef = this;
+    setTimeout(function() {
+      thisRef.setState({ bytecodeButtonText: initialBytecodeButtonText });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="SearchResultContainer">
+        <Card>
+          <CardTitle
+            title={this.props.contract.name || "Contract"}
+            subtitle={this.props.contract.address}
+          />
+          <CardActions>
+            <FlatButton label="Upload Source" />
+            <FlatButton label="Send Transaction" />
+            <CopyToClipboard text={this.props.contract.code}>
+              <FlatButton
+                label={this.state.bytecodeButtonText}
+                onClick={this.copyBytecodeClicked}/>
+            </CopyToClipboard>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default SearchResult;
