00a73b69d82b37b0be736a2f0f3caa30f1e75cf2
Antonio Juliano <antonio.m.juliano@gmail.com>
2017-04-22 18:23:29 -0700

    dont send metrics in dev

--- client/src/lib/Mixpanel.js
+++ client/src/lib/Mixpanel.js
@@ -1,3 +1,11 @@
 /* eslint-disable */
-
-export default mixpanel;
+let toExport;
+if (process.env.NODE_ENV === 'production') {
+  toExport = mixpanel;
+} else {
+  toExport = {
+    track: t => null,
+    identify: t => null
+  }
+}
+export default toExport;
